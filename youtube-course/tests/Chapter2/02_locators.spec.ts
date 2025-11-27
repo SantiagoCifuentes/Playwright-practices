0ed4213c997eec642a50e65af6c904cc3101ab44
santcial <santiago.ci9619@gmail.com>
2025-11-26 21:15:42 -0500

    locators

--- youtube-course/tests/Chapter2/02_locators.spec.ts
+++ youtube-course/tests/Chapter2/02_locators.spec.ts
@@ -2,12 +2,28 @@ import {test, expect} from  '@playwright/test'
 
 test('Locators in Playwright', async ({page}) => {
 
-    await page.goto('https://github.com/SantiagoCifuentes')
+    // await page.goto('https://github.com/SantiagoCifuentes')
 
     // await page.getByRole('link', {name: 'Sign in'}).click();
    
-    await page.getByLabel('Homepage', {exact: true}).first().click();
+    // await page.getByLabel('Homepage', {exact: true}).first().click();
 
-   
+    // await page.getByAltText("View SantiagoCifuentes's full-sized avatar").click();
+
+    // await page.getByTestId('repositories').first().click();
+
+//    await page.getByText('Sign up').click();
+
+    // await page.goto('https://www.youtube.com/')
+    // await page.getByPlaceholder('Search').click();
+    // await page.getByPlaceholder('Search').fill('Playwright');
+    // await page.getByPlaceholder('Search').press('Enter');
+
+    // await page.locator('//input[@name="search_query"]').fill('Playwright');
+    //  await page.locator('input[name="search_query"]').fill('Playwright');
     
+
+
+    await page.goto('https://www.google.com/');
+    await page.getByTitle('Buscar').fill('Playwright');
 });
