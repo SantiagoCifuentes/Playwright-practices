938e35435e21ff9be9a8a813bdddcd41d661acd3
Santiago <santiago.cifuentes@sofka.com.co>
2026-01-06 16:03:56 -0500

    assertions

---
+++ youtube-course/tests/Chapter2/09_assertions.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test'
+
+test(' Assertions ', async ({ page }) => {
+
+  //enter action
+  await page.goto('https://www.youtube.com/');
+
+
+  //visible, editable, enabled, empty
+  await expect(page.getByPlaceholder('Search', { exact: true }).first()).toBeVisible();
+  await expect(page.getByPlaceholder('Search', { exact: true }).first()).toBeEditable();
+  await expect(page.getByPlaceholder('Search', { exact: true }).first()).toBeEnabled();
+  await expect(page.getByPlaceholder('Search', { exact: true }).first()).toBeEmpty();
+
+
+  //verify url, page title, text, number of elements(count)
+
+  await page.getByPlaceholder('Search', { exact: true }).first().click();
+  await page.getByPlaceholder('Search', { exact: true }).first().fill('playwright by testers talk');
+  await page.getByPlaceholder('Search', {exact: true}).first().press('Enter');
+  await expect(page).toHaveURL('https://www.youtube.com/results?search_query=playwright+by+testers+talk');
+
+  await expect(page).toHaveTitle('playwright by testers talk - YouTube')
+
+});
